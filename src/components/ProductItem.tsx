4f15478a2b58bf2185dd34cfa2acec4cf0b7c36b
Quyet Tran <quyet.trantien0402@gmail.com>
2022-08-05 14:49:08 +0700

    feat: add favourite tag

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -2,7 +2,14 @@ import { Rating } from './UI/Rating'
 
 export const ProductItem = ({ productItem }: { productItem: any }) => {
   return (
-    <div className='border border-rose-500 rounded p-2'>
+    <div className='relative border border-rose-500 rounded p-2'>
+      <p
+        className={`absolute top-3 left-0 bg-orange-600 px-1 rounded-r-sm text-white ${
+          productItem.rating?.rate >= 4 ? '' : 'hidden'
+        }`}
+      >
+        Favourite
+      </p>
       <img
         className='w-full h-96 object-fill'
         src={productItem.image}

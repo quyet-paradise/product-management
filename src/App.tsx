1055a8c459f95f063010f35dda59df52831bf748
Quyet Tran <quyet.trantien0402@gmail.com>
2022-08-08 16:08:13 +0700

    feat: add product detail modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import { Fragment, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { axiosClient } from './service/axiosClient'
 import { ProductItem } from './components/ProductItem'
 import { SelectBox } from './components/UI/SelectBox'
+import { DefaultModal } from './components/Modal/DefaultModal'
+
+import { Product } from './type/product.type'
 
 export const App = () => {
+  const initProduct: Product = {
+    id: 1,
+    title: '',
+    price: 0,
+    category: '',
+    description: '',
+    image: '',
+    rating: {
+      rate: 0,
+      count: 0,
+    },
+  }
+
   const [products, setProducts] = useState([])
 
+  const [product, setProduct] = useState(initProduct)
+
   const [categories, setCategories] = useState([])
+
+  const [showModal, setShowModal] = useState(false)
 
   const handleChangeCategory = (category: string) => {
     if (category === 'all') {
@@ -21,6 +41,11 @@ export const App = () => {
           setProducts(res.data)
         })
     }
+  }
+
+  const handleToggleModal = (product: Product) => {
+    setProduct(product)
+    setShowModal(true)
   }
 
   useEffect(() => {
@@ -44,11 +69,16 @@ export const App = () => {
         categories={categories}
         handleChangeCategory={handleChangeCategory}
       />
-      {products.map((product: any) => (
-        <Fragment key={product.id}>
+      {products.map((product: Product) => (
+        <a key={product.id} onClick={() => handleToggleModal(product)}>
           <ProductItem productItem={product} />
-        </Fragment>
+        </a>
       ))}
+      <DefaultModal
+        isShow={showModal}
+        onClose={() => setShowModal(false)}
+        product={product}
+      />
     </div>
   )
 }
